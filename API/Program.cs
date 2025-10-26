109a78616ae98f232cd97d6318401c0e4847eac0
admin <hoangthaisonqs@gmail.com>
2025-10-27 01:08:50 +0700

    init api order

--- API/Program.cs
+++ API/Program.cs
@@ -1,21 +1,28 @@
+﻿using BUS.Services;
+using BUS.Services.Interfaces;
 using DAL;
+using DAL.Repository;
+using DAL.RepositoryAsyns;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped(typeof(IRepositoryAsync<>), typeof(RepositoryAsync<>));
+
+builder.Services.AddTransient<IOrderServices, OrderServices>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,9 +30,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ BUS/Services/Interfaces/IOrderServices.cs
@@ -0,0 +1,11 @@
+﻿using DAL.DTOs;
+using DAL.Entities;
+using DAL.Models;
+
+namespace BUS.Services.Interfaces
+{
+    public interface IOrderServices
+    {
+        Task<CommonResponse<bool>> CreateOrder(CreateOrderDTO createOrder);
+    }
+}
