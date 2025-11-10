53b374e0d936b361c6f4efddc66e6ad2ac5cd3f0
sonnptph49000-byte <sonnptph49000@gmail.com>
2025-11-10 11:40:30 +0700

    Add files via upload
    crud admin product

--- AdminWeb/Program.cs
+++ AdminWeb/Program.cs
@@ -1,11 +1,22 @@
-using AdminWeb;
+﻿using AdminWeb;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using AdminWeb.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Cấu hình HttpClient cho AdminWeb API
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Thêm ProductService vào DI container
+builder.Services.AddScoped<ProductService>();
+
+// Nếu cần sử dụng API base URL từ cấu hình
+// builder.Services.AddHttpClient<ProductService>(client =>
+// {
+//     client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]);
+// });
 
 await builder.Build().RunAsync();
